bd856ab8b6f3deee1eed59183f1be0c6565addfc
ashish-511 <ashishpatil1049@gmail.com>
2022-09-12 23:13:33 +0530

    front end react app - signin complete

---
+++ frontend/src/lib/apiList.js
@@ -0,0 +1,15 @@
+export const server = "http://localhost:8080";
+
+const apiList = {
+  login: `${server}/user/signin`,
+  signup: `${server}/user/signup`,
+  uploadResume: `${server}/upload/resume`,
+  uploadProfileImage: `${server}/upload/profile`,
+  jobs: `${server}/api/jobs`,
+  applications: `${server}/api/applications`,
+  rating: `${server}/api/rating`,
+  user: `${server}/api/user`,
+  applicants: `${server}/api/applicants`,
+};
+
+export default apiList;
